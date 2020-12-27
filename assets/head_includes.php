e1acadb95fb1b3a26d66791724f743781abc7d4e
Ahmed <ahmedmostafax288@gmail.com>
2020-12-27 13:01:49 +0200

    Add Starting template

---
+++ assets/head_includes.php
@@ -0,0 +1,15 @@
+<?php $base_url = "http://localhost/git-wiki/"; ?>
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="<?php echo $base_url; ?>frameworks/css/bootstrap.min.css">
+
+<!-- Animate-on-scroll CSS -->
+<link rel="stylesheet" href="<?php echo $base_url; ?>frameworks/css/aos.css" />
+
+<!-- Font Awesome JS -->
+<script defer src="<?php echo $base_url; ?>frameworks/fontawesome/js/solid.js"></script>
+<script defer src="<?php echo $base_url; ?>frameworks/fontawesome/js/fontawesome.js"></script>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+    
+    <head>
+        <title>Tracking Expenses</title>
+        
+        <?php include 'assets/base.php'; ?>
+        <?php include 'assets/head_includes.php'; ?>
+
+    </head>
+
+    <body>
+
+        <!-- Whole page contenet -->
+        <div class="container-fluid p-0">
+
+            <?php include "content.php"; ?>
+            <?php include "assets/footer.php"; ?>
+
+        </div> <!-- End of the primary container -->
+
+        <?php include 'assets/scripts.php'; ?>
+        <!-- scripts -->
+    </body>
+
+</html>
