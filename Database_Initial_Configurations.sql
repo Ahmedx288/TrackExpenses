c0872066e2f82afacc768faebe75ca4da1268220
Ahmed <ahmedmostafax288@gmail.com>
2020-12-30 01:13:12 +0200

    Update database schema

--- Database_Initial_Configurations.sql
+++ Database_Initial_Configurations.sql
@@ -1,20 +1,21 @@
-drop database if exists expense_tracking;
-create database if not exists expense_tracking;
-use expense_tracking;
+DROP DATABASE IF EXISTS expense_tracking;
+CREATE DATABASE IF NOT EXISTS  expense_tracking;
+USE expense_tracking;
 
 DROP TABLE IF EXISTS vendor;
 CREATE TABLE vendor (
-    id INT, 
-	name_ VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT,
+	name_ VARCHAR(255) NOT NULL,
     
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS vendor_location; ##!!!!!!
 CREATE TABLE vendor_location (
-    vendor_id INT,
-    address VARCHAR(255),
-	city VARCHAR(255),
+    vendor_id INT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+	city VARCHAR(255) NOT NULL,
+    address_notes VARCHAR(255),
     
     PRIMARY KEY (vendor_id, address),
     FOREIGN KEY (vendor_id) REFERENCES vendor(id) ON DELETE CASCADE
@@ -22,8 +23,11 @@ CREATE TABLE vendor_location (
 
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (
-    id INT, 
-	name_ VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT, 
+	first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender ENUM('male', 'female') NOT NULL,
     
     PRIMARY KEY (id)
 );
@@ -31,7 +35,7 @@ CREATE TABLE customer (
 DROP TABLE IF EXISTS customer_phone;
 CREATE TABLE customer_phone (
     customer_id INT, 
-    phone_num CHAR(11) CHECK (phone_num LIKE '01\d{9}'),
+    phone_num CHAR(11),
     
     PRIMARY KEY (customer_id , phone_num),
     FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
@@ -39,31 +43,31 @@ CREATE TABLE customer_phone (
 
 DROP TABLE IF EXISTS category;
 CREATE TABLE category (
-	id INT, 
-    main_category VARCHAR(255),
-    sub_category VARCHAR(255),
+	id INT NOT NULL AUTO_INCREMENT, 
+    main_category VARCHAR(255) NOT NULL,
+    sub_category VARCHAR(255) NOT NULL,
     
 	PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS product;
 CREATE TABLE product (
-	id INT, 
-    name_ VARCHAR(255),
-    trade_mark VARCHAR(255),
-    category_id INT,
-    weight_liter DECIMAL, #Kg VS L
+	id INT NOT NULL AUTO_INCREMENT,
+    name_ VARCHAR(255) NOT NULL,
+    trade_mark VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL,
+    weight_liter FLOAT NOT NULL, #Kg VS L | Weight_Liter = 0 -> N/A (Not Applicable);
     
     PRIMARY KEY (id),
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
-);
+); #this table is used only to describe the product characteristics itself, not the price or anything else.
 
 
 DROP TABLE IF EXISTS customer_rating;
 CREATE TABLE customer_rating ( 
-	customer_id INT, 
-    product_id INT,
-    rate INT CHECK (rate >= 0),
+	customer_id INT NOT NULL, 
+    product_id INT NOT NULL,
+    rate TINYINT UNSIGNED NOT NULL,
     comments VARCHAR(255),
     
     PRIMARY KEY (customer_id , product_id),
@@ -73,13 +77,13 @@ CREATE TABLE customer_rating (
 
 DROP TABLE IF EXISTS invoice;
 CREATE TABLE invoice (
-    id INT,
-    customer_id INT,
-    vendor_id INT,
-    payment_date DATE,
-    payment_time Time,
-    #-----> total_payment DECIMAL,
-    payment_method VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    vendor_id INT NOT NULL,
+    payment_date DATE NOT NULL,
+    payment_time Time NOT NULL,
+    total_payment DOUBLE, #Trigger (sum(invoice items))
+    payment_method ENUM('cash', 'electronic'),
     
     PRIMARY KEY (id),
     FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE,
@@ -90,9 +94,10 @@ DROP TABLE IF EXISTS invoice_item;
 CREATE TABLE invoice_item ( 
 	invoice_id INT, 
     product_id INT,
-    price DOUBLE CHECK (price > 0),
-    quantity INT CHECK (quantatiy > 0),
-    discount DOUBLE CHECK (discount >= 0),
+    price DOUBLE UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    total_discount DOUBLE UNSIGNED, 
+    total_pay DOUBLE UNSIGNED, #Trigger [(price * quantity) - total_discount];
     
     PRIMARY KEY (invoice_id , product_id),
     FOREIGN KEY (invoice_id) REFERENCES invoice(id) ON DELETE CASCADE,
@@ -101,24 +106,26 @@ CREATE TABLE invoice_item (
 	
 
 
-INSERT INTO vendor
+INSERT INTO vendor (name_)
 VALUES
-	(1, 'Khair-Zaman'),
-    (2, 'Buffalo Burger'),
-    (3, 'Ashraf Farghaly Juice');
+	('Khair-Zaman'),
+    ('Buffalo Burger'),
+    ('Ashraf Farghaly Juice');
 
 INSERT INTO vendor_location
 VALUES
-	(1, 'Yousri Raghib', 'Assiut'),
-    (1, 'Asmaa-allah alhosna', 'Assiut'),
-    (2, 'Republic street', 'Assiut'),
-    (3, 'Asmaa-allah alhosna', 'Assiut');
+	(1, 'Yousri Raghib', 'Assiut', ''),
+    (1, 'Asmaa-allah alhosna', 'Assiut', ''),
+    (2, 'Republic street', 'Assiut', ''),
+    (3, 'Asmaa-allah alhosna', 'Assiut', '');
 
-INSERT INTO customer
+INSERT INTO customer (first_name, last_name, age, gender)
 VALUES
-	(1, 'Ahmed'),
-    (2, 'Eslam'),
-    (3, 'Hossam');
+		('Ahmed', 'Mostafa', 22, 'male'),
+        ('Hossam', 'Ahmed', 22, 'male'),
+        ('Eslam', 'Taha', 22, 'male'),
+        ('Maryam', 'Mostafa', 20, 'female');
+
 
 INSERT INTO customer_phone
 VALUES
@@ -127,43 +134,64 @@ VALUES
     (2, '01111111111'),
     (3, '01555555555');
 
-INSERT INTO category
+INSERT INTO category (main_category, sub_category)
 VALUES
-	(1, 'Food', 'Dairy'),
-    (2, 'Food', 'Snacks'),
-    (3, 'Food', 'Fast Food'),
-    (4, 'Food', 'Drinks'),
-    (5, 'Health', 'Personal hygiene');
+	('Food', 'Dairy'),
+    ('Food', 'Snacks'),
+    ('Food', 'Fast Food'),
+    ('Food', 'Drinks'),
+    ('Health', 'Personal hygiene');
 
-INSERT INTO product
+INSERT INTO product (name_ , trade_mark, category_id, weight_liter)
 VALUES
-	(1, 'Trash Bags', 'Khair-Zman', 5, 25),
-    (2, 'Juhayna milk', 'Khair-Zman', 1, 1),
-    (3, 'Chocolate', 'Cadbury', 2, 0.090),
-    (4, 'Burger offers', 'Buffalo Burger', 3, 1),
-    (5, 'Mango Juice', 'Farghaly', 4, 0), #Weight_Liter = 0 -> N/A
-    (6, 'Mango+Peache Juice', 'Farghaly', 4, 0),
-    (7, 'Apple Juice', 'Farghaly', 4, 0);
+	('Trash Bags', 'Khair-Zman', 5, 25),
+    ('Juhayna milk', 'Khair-Zman', 1, 1),
+    ('Chocolate', 'Cadbury', 2, 0.090),
+    ('Burger offers', 'Buffalo Burger', 3, 1),
+    ('Mango Juice', 'Farghaly', 4, 0), #Weight_Liter = 0 -> N/A
+    ('Mango+Peache Juice', 'Farghaly', 4, 0),
+    ('Apple Juice', 'Farghaly', 4, 0);
 
-INSERT INTO invoice
+INSERT INTO customer_rating
 VALUES
-	(1, 1, 1, DATE '2020-09-04', '07:54:35', 'Cash'),#AHMED, Buys from Khair-zman on invoice 1
-    (2, 3, 2, DATE '2020-11-04', '10:45:17', 'Visa'),#Hossam, Buys from Buffalo on invoice 2
-    (3, 2, 3, DATE '2020-09-07', '11:30:28', 'Cash');#Eslam, Buys from Farghaly on invoice 3
+	   (1 , 2, 5, 'A'), 	#Ahmed, about Juhayna milk, rate 5
+	   (2 , 2, 2, 'F'),		#Eslam, about Juhayna milk, rate 2
+       (3 , 2, 3, 'C');		#Hosam, about Juhayna milk, rate 3
+
+INSERT INTO invoice (customer_id, vendor_id, payment_date, payment_time, payment_method)
+VALUES
+	(1, 1, DATE '2020-09-04', '07:54:35', 'cash'),			#AHMED, Buys from Khair-zman on invoice 1
+    (3, 2, DATE '2020-11-04', '10:45:17', 'electronic'),	#Hossam, Buys from Buffalo on invoice 2
+    (2, 3, DATE '2020-09-07', '11:30:28', 'Cash');			#Eslam, Buys from Farghaly on invoice 3
     
 INSERT INTO invoice_item
 VALUES
-	(1, 1, 19.99, 1, 0), #invoice 1, item_1(Trash Bags), price, quantity, discount
-    (1, 2, 26, 1, 0), #invoice 1, item_2(Juhayna milk), price, quantity, discount
-    (1, 3, 15.99, 1, 0), #invoice 1, item_3(Choclate), price, quantity, discount
+	(1, 1, 19.99, 1, 0), 	#invoice 1, item_1(Trash Bags), price, quantity, discount
+    (1, 2, 26, 1, 0), 		#invoice 1, item_2(Juhayna milk), price, quantity, discount
+    (1, 3, 15.99, 1, 0), 	#invoice 1, item_3(Choclate), price, quantity, discount
     
-    (2, 4, 153.5, 1, 40), #invoice 2
+    (2, 4, 153.5, 1, 40), 	#invoice 2
     
-    (3, 5, 13, 1, 0), #invoice 3
-    (3, 6, 16, 1, 0), #invoice 3
-    (3, 7, 14, 1, 0); #invoice 3
+    (3, 5, 13, 1, 0), 		#invoice 3
+    (3, 6, 16, 1, 0), 		#invoice 3
+    (3, 7, 14, 1, 0); 		#invoice 3
 
 Select invoice.customer_id, invoice.id as 'invoice #', sum(price) As 'Total', invoice.payment_method 
-from invoice JOIN invoice_item
+FROM invoice JOIN invoice_item
 ON invoice.id = invoice_item.invoice_id
-group by invoice.id;
+GROUP BY invoice.id;
+
+SELECT customer.first_name, product.name_, customer_rating.rate, customer_rating.comments
+FROM customer_rating JOIN customer JOIN product
+ON customer_rating.customer_id = customer.id AND customer_rating.product_id = product.id;
+
+SELECT invoice.id AS 'invoice num #', customer.first_name AS 'buyer', vendor.name_ AS 'market', payment_method
+FROM customer JOIN vendor JOIN invoice
+ON customer.id = invoice.customer_id AND invoice.vendor_id = vendor.id;
+
+SELECT
+invoice.id AS 'invoice num #', customer.first_name AS 'buyer', vendor.name_ AS 'market',
+product.name_, payment_method
+FROM customer JOIN vendor JOIN invoice JOIN invoice_item JOIN product
+ON customer.id = invoice.customer_id AND invoice.vendor_id = vendor.id AND 
+invoice_item.invoice_id = invoice.id AND product.id = invoice_item.product_id;
--- README.md
+++ README.md
@@ -22,21 +22,27 @@ This picture of many products that have been purchased at some time earlier cont
 and many other useful questions that make the user aware of his expenses and where his money goes.
 ___
 ## ERD
-![Conceptual Model](images/Conceptual%20Model_V3.png)
+![Conceptual Model](images/Conceptual%20Model_V4.png)
 ___
-## Logical Model
-Databas table reduced to the thrid form of normalization process
-|#|Table|Attributes|
-|--|--|--|
-|1|vendor|(**id**, name_)|
-|2|vendor_location|(**vendor_id, address**, city)|
-|3|customer|(**id**, name_)|
-|4|customer_phone|(**customer_id, phone_num**)|
-|5|category|(**id**, main_category, sub_category)|
-|6|product|(**id**, name_, trade_mark, **category_id**, weight_liter) |
-|7|customer_rating|(**customer_id, product_id**, rate, comments)|
-|8|invoice|(**id**, **customer_id, vendor_id**, payment_date, payment_time, total_pay, payment_method)|
-|9|invoice_item|(**invoice_id, product_id**, price, quantity, discount)|
+## Logical Model (Compact)
+Database tables reduced to the thrid form of normalization process
+|#|Table|Attributes|Keys
+|--|--|--|--|
+|1|vendor|(**id**, name_)|PK: id|
+|2|vendor_location|(**vendor_id**, **address**, city, address_notes)|PK: (vendor_id, address) |
+| |               |                                                 |FK: vendor_id->vendor(id)|
+|3|customer|(**id**, first_nameو last_name, age, gender)|PK: id|
+|4|customer_phone|(**customer_id**, **phone_num**)|PK: (customer_id, phone_num) |
+| |               |                               |FK: customer_id->customer(id)|
+|5|category|(**id**, main_category, sub_category)|PK: id|
+|6|product|(**id**, name_, trade_mark, **category_id**, weight_liter) |PK: id                       |
+| |               |                                                   |FK: category_id->category(id)|
+|7|customer_rating|(**customer_id**, **product_id**, rate, comments)|PK: (customer_id, product_id)                         |
+| |               |                                                 |FK: customer_id->customer(id), product_id->product(id)|
+|8|invoice|(**id**, **customer_id**, **vendor_id**, payment_date, payment_time, total_pay, payment_method)|PK: id                                              |
+| |               |                                                                                       |FK: customer_id->customer(id), vendor_id->vendor(id)|
+|9|invoice_item|(**invoice_id**, **product_id**, price, quantity, total_discount, total_pay)|PK: (invoice_id, product_id)                        |
+| |               |                                                                         |FK: invoice_id->invoice(id), product_id->product(id)|
 ___
 **Installations:**\
 You only need a server to host the website files and database to connect to it.
